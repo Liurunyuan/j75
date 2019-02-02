2b069b08f5f44e412f2be652835131f042dc377d
Simon <unknown@example.com>
2019-02-02 19:47:58 +0800

    fix bug

--- source_dsp/DSP280x_Gpio.c
+++ source_dsp/DSP280x_Gpio.c
@@ -52,7 +52,6 @@ void InitGpio(void)
    //GpioCtrlRegs.GPAPUD.all = 0xFFFF;    // Pullup's disabled GPIO0-GPIO31
    //GpioCtrlRegs.GPBPUD.all = 0xFFFF;    // Pullup's disabled GPIO32-GPIO34
    EDIS;
-   sdfsf
 
 }	
 
