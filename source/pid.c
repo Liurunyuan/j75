fef1b109e3faf050c5745f9009cfa4afbe745a7a
Gang <lug@shrz.com>
2019-06-16 20:08:30 +0800

    optimize break logic

--- source/pid.c
+++ source/pid.c
@@ -30,6 +30,8 @@ int16 PidOutput(double currentSpeed){
 	int16 pidOutput = 0;
 	int32 ek1;
 	int16 dis_temp;
+	int16 maxbreak;
+	int16 minbreak;
 
 	ek1 = (int32)(gTargetSpeed - currentSpeed);
 	if((ek1 > -gSysInfo.thresholdKiError) && (ek1 < gSysInfo.thresholdKiError))
@@ -56,22 +58,34 @@ int16 PidOutput(double currentSpeed){
 
 	/*break distance*/
 	dis_temp = ((int16)ek1) >> 2;
-	if(MAXBREAKDISTANCE < dis_temp){
-		dis_temp = MAXBREAKDISTANCE;
+	if(2000 > gTargetSpeed){
+		maxbreak = MAXBREAKDISTANCE;
+		minbreak = MINBREAKDISTANCE;
 	}
-	else if(MINBREAKDISTANCE > dis_temp){
-		dis_temp = MINBREAKDISTANCE;
+	else if(4000 > gTargetSpeed){
+		maxbreak = MAXBREAKDISTANCE >> 2;
+		minbreak = MINBREAKDISTANCE >> 2;
+	}
+	else{
+		maxbreak = MAXBREAKDISTANCE >> 3;
+		minbreak = MINBREAKDISTANCE >> 3;
+	}
+	if(maxbreak < dis_temp){
+		dis_temp = maxbreak;
+	}
+	else if(minbreak > dis_temp){
+		dis_temp = minbreak;
 	}
 	if((300 < ek1) || (-300 > ek1)){
 		gSysInfo.breakDistance = dis_temp;
 	}
 	else{
 		dis_temp = gSysInfo.breakDistance;
-		if(10 < dis_temp){
-			dis_temp = dis_temp - 10;
+		if(3 < dis_temp){
+			dis_temp = dis_temp - 3;
 		}
-		else if(-10 > dis_temp){
-			dis_temp = dis_temp + 10;
+		else if(-3 > dis_temp){
+			dis_temp = dis_temp + 3;
 		}
 		else{
 			dis_temp = 0;
